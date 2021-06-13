e121ec091c2ca2237074e3f6e3e0c268bd8aaa04
iyukukuku <nurruddinafif16@github.com>
2021-06-13 16:28:57 +0700

    Finish UI for login Page

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
-        <link href="login.css" rel="stylesheet">
+        <link href="css/login.css" rel="stylesheet">
         <style>
             Body {
                 background: url(https://assets.hongkiat.com/uploads/minimalist-dekstop-wallpapers/4k/original/10.jpg);
@@ -11,10 +11,11 @@
         </style>
     </head>
         <form class="form">
+            <img src="https://img.icons8.com/nolan/64/virus.png"/>
             <h1>pandENDmic</h1>
             <h3>ProjectBy_2039_2013</h3>
             <div class="form-floating mb-3">
-                <input type="text" class="form-control" id="floatingInput" placeholder="name@example.com" name="username" value="<?php echo $username ?>">
+                <input type="text" class="form-control" id="floatingInput" placeholder="name@example.com" name="username">
                 <label for="floatingInput">Username</label>
               </div>
               <div class="form-floating">
@@ -23,12 +24,11 @@
               </div> 
               <div class="form-check">
                 <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" name="rememberme">
-                <?php if($rememberme == '1') echo "checked" ?>
                 <label class="form-check-label" for="flexCheckDefault">
                   Remember Me?
                 </label>
               </div>
-              <input type="Submit" class="btn btn-primary" name="login" value="Login"/>
+              <button type="button" class="btn btn-primary">Login</button>
         </form>
     </body>
 </html>
