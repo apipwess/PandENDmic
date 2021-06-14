2777209392dd419db2271a255f9fd53cbbd72a61
ZinedineR <58017453+ZinedineR@users.noreply.github.com>
2021-06-14 22:08:49 +0700

    MVC Initialization
    waiting for the Afif's update
    
    Co-Authored-By: iyukukuku <71748301+iyukukuku@users.noreply.github.com>

---
+++ php/login.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+        <link href="assets/css/login.css" rel="stylesheet">
+        <style>
+            Body {
+                background: url(https://assets.hongkiat.com/uploads/minimalist-dekstop-wallpapers/4k/original/10.jpg);
+                background-size: cover;
+            }
+        </style>
+    </head>
+        <form class="form">
+            <img src="https://img.icons8.com/nolan/64/virus.png"/>
+            <h1>pandENDmic</h1>
+            <h3>ProjectBy_2039_2013</h3>
+            <div class="form-floating mb-3">
+                <input type="text" class="form-control" id="floatingInput" placeholder="name@example.com" name="username">
+                <label for="floatingInput">Username</label>
+              </div>
+              <div class="form-floating">
+                <input type="password" class="form-control" id="floatingPassword" placeholder="Password" name="password">
+                <label for="floatingPassword">Password</label>
+              </div> 
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" name="rememberme">
+                <label class="form-check-label" for="flexCheckDefault">
+                  Remember Me?
+                </label>
+              </div>
+              <button type="button" class="btn btn-primary">Login</button>
+        </form>
+    </body>
+</html>
