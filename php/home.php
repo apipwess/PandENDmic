9e7c403a685cd195c3bba17bca2bae00449fe292
iyukukuku <nurruddinafif16@github.com>
2021-06-15 22:08:32 +0700

    Finishing Home UI

--- php/home.php
+++ php/home.php
@@ -3,7 +3,7 @@
     <head>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
         <link href="../assets/css/home.css" rel="stylesheet">
-        <meta charset="utf-8">
+        <meta charset="UTF-8">
         <style>
             Body {
                 background: url(https://assets.hongkiat.com/uploads/minimalist-dekstop-wallpapers/4k/original/10.jpg);
@@ -12,7 +12,7 @@
         </style>
     </head>
     <header>
-      <form class="form">
+        <form class="form">
         <img src="https://img.icons8.com/nolan/96/virus.png"/>
         <div class="animasi">pandENDmic</div>
         <h3>"All the stories about us who got through this pandemic and all the messages that haven't been conveyed in this pandemic too"</h3>
