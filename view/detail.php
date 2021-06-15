979175a1d3da4234595ec60bc9695b77a2858209
iyukukuku <nurruddinafif16@github.com>
2021-06-15 20:10:23 +0700

    update detail.php

--- view/detail.php
+++ view/detail.php
@@ -1,8 +1,7 @@
 <body>
     <?php foreach($story as $row): ?>
     <form class="form2">
-        <h1>to : Me</h1>
-        <h1>from : <?=$row['from_who'] ?> </h1>
+        <h1>to : <?=$row['from_who'] ?> </h1>
         <h3>"<?=$row['messages'] ?>..."</h3>
     <?php endforeach ?>
     </body>
